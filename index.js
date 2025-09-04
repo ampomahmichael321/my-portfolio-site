b1c96743c1b99d48fcc2b03e04511aa9e8049327
Michael Ampomah <162718355+ampomahmichael321@users.noreply.github.com>
2025-09-04 23:45:03 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let hireMeBtn = document.querySelector(".hire-me-button");
+
+hireMeBtn.addEventListener("click", () => {
+  document
+    .querySelector(".contact-section")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+// Select all elements you want to animate on scroll
+const animatedElements = document.querySelectorAll(
+  ".landing-page, .about, .skills, .services, .contact-section,nav"
+);
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+      } else {
+        entry.target.classList.remove("animate");
+      }
+    });
+  },
+  {
+    threshold: 0.2, // Adjust as needed
+  }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
