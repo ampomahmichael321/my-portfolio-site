f71d69a8ea8732e471c5bb1f21a937443edb8b98
Michael Ampomah <162718355+ampomahmichael321@users.noreply.github.com>
2025-09-05 17:20:10 +0000

    jbk

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ hireMeBtn.addEventListener("click", () => {
 
 // Select all elements you want to animate on scroll
 const animatedElements = document.querySelectorAll(
-  ".landing-page, .about, .skills, .services, .contact-section,nav, label, .button,"
+  ".landing-page, .about, .skills, .services, .contact-section,nav, label"
 );
 
 const observer = new IntersectionObserver(
