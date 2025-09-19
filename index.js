21526056d18d5bfb40f8532260d886093db31e58
Michael Ampomah <162718355+ampomahmichael321@users.noreply.github.com>
2025-09-19 13:05:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const menuBtn = document.querySelector(".menu");
 const hiddenMenu = document.querySelector(".hidden-menu");
+const navLinks = document.querySelectorAll(".link");
 menuBtn.addEventListener("click", () => {
   hiddenMenu.style.display = "flex";
 });
 const closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener("click", () => {
+  hiddenMenu.style.display = "none";
+});
+
+navLinks.addEventListener("click", () => {
   hiddenMenu.style.display = "none";
 });
