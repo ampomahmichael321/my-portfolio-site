989f41ec546238b60a01146a3b8bed3d45d2de13
Michael Ampomah <162718355+ampomahmichael321@users.noreply.github.com>
2025-09-24 17:14:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const navLinks = document.querySelectorAll(".link");
 
 AOS.init();//Initialize animate on scroll library
 
+const serviceCards = document.querySelectorAll(".service");
+serviceCards.forEach(service =>{
+    service.setAttribute("data-aos", "fade-up");
+  service.setAttribute("data-aos-duration", "1500")
+})
 
 menuBtn.addEventListener("click", () => {
   hiddenMenu.style.transform = "translateX(0)";
@@ -18,6 +23,7 @@ navLinks.forEach(link => {
     hiddenMenu.style.transform = "translateX(100%)";
   });
 });
+
 
 
 
